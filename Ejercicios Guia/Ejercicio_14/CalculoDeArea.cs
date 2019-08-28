42a25106983fc75242d74a2d197c4ae21197a71c
matiashamie <mnhamie@gmail.com>
2019-08-28 19:53:15 -0300

    Ejercicios 13,14,15 ok

---
+++ Ejercicios Guia/Ejercicio_14/CalculoDeArea.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_14
+{
+    class CalculoDeArea
+    {
+        public static double CalcularCuadrado(double lado)
+        {
+            return lado * lado;
+        }
+
+        public static double CalcularTriangulo(double b, double h)
+        {
+            return (b * h) / 2;
+        }
+
+        public static double CalcularCirculo(double radio)
+        {
+            return Math.PI * (radio * radio);
+        }
+    }
+}
---
+++ Ejercicios Guia/Ejercicio_15/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 15. Realizar un programa que permita realizar operaciones matemáticas simples (suma, resta,
+ * multiplicación y división). Para ello se le debe pedir al usuario que ingrese dos números y la
+ * operación que desea realizar (pulsando el caracter +, -, * ó /).
+ * El usuario decidirá cuándo finalizar el programa.
+ * Crear una clase llamada Calculadora que posea dos métodos estáticos (de clase):
+ * a. Calcular (público): Recibirá tres parámetros, el primer número, el segundo número y la
+ * operación matemática. El método devolverá el resultado de la operación.
+ * b. Validar (privado): Recibirá como parámetro el segundo número. Este método se debe
+ * utilizar sólo cuando la operación elegida sea la DIVISIÓN. Este método devolverá
+ * TRUE si el número es distinto de CERO.
+ */
+
+namespace Ejercicio_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char opcion = ' ';
+            char seguir = 'S';
+            string cadena = "";
+            double primero = 0, segundo = 0, resultado = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("** Calculadora **");
+
+                Console.Write("Ingrese el primer valor: ");
+                while(!double.TryParse(Console.ReadLine(),out primero))
+                {
+                    Console.Write("ERROR, Reingrese el primer numero: ");
+                }
+                
+                Console.Write("Ingrese el segundo valor: ");
+                while (!double.TryParse(Console.ReadLine(), out segundo))
+                {
+                    Console.Write("ERROR, Reingrese el segundo numero: ");
+                }
+
+                Console.Clear();
+                Console.WriteLine("+ -> Para sumar");
+                Console.WriteLine("- -> Para restar");
+                Console.WriteLine("* -> Para multiplicar");
+                Console.WriteLine("/ -> Para dividir");
+
+                Console.Write("Ingrese el operador deseado: ");
+                while (!char.TryParse(Console.ReadLine(),out opcion))
+                {
+                    Console.Write("ERROR, Reingrese opcion");
+                }
+
+                resultado = Calculadora.Calcular(primero, segundo, opcion);
+
+                if (resultado != -999.999)
+                {
+                    Console.Clear();
+                    Console.WriteLine("El resultado de {0} {1} {2} = {3}", primero, opcion, segundo, resultado);
+                }
+
+                Console.ReadKey();
+
+            } while (seguir=='S');
+        }
+    }
+}
