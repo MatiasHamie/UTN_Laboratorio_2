7695a9fe9e4cd60043d8ad44a9902eed90f35528
matiashamie <mnhamie@gmail.com>
2019-08-29 22:09:09 -0300

    Ejercicio 18 (lo empece)

---
+++ Ejercicios Guia/Ejercicio_1/Ejercicio_18/PruebaGeometria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    class Punto
+    {
+        private int x;
+        private int y;
+
+        public int GetX()
+        {
+            return this.x;
+        }
+
+        public int GetY()
+        {
+            return this.y;
+        }
+    }
+}
