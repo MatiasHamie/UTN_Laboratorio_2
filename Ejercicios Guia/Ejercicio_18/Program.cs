fef287bb740ec9f8d08d50fce9c29471cee761a7
matiashamie <mnhamie@gmail.com>
2019-09-02 12:18:15 -0300

    Ejercicio 18 terminado

---
+++ Ejercicios Guia/Ejercicio_18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Geometria;
+
+/*
+ *    v1-----------v2
+ *    |             |
+ *    |             |
+ *    v4-----------v3
+ */
+namespace PruebaGeometria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto punto1 = new Punto(0, 5);
+            Punto punto3 = new Punto(10, 0);
+
+            Rectangulo rectangulo1 = new Rectangulo(punto1, punto3);
+
+            rectangulo1.Area();
+            rectangulo1.Perimetro();
+
+            Console.WriteLine("Area: {0} Perímetro: {1}",rectangulo1.GetArea(),rectangulo1.GetPerimetro());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios Guia/Ejercicio_18/Rectangulo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+ *    v1-----------v2
+ *    |             |
+ *    |             |
+ *    v4-----------v3
+ */
+
+namespace Geometria
+{
+    class Rectangulo
+    {
+        private float area, perimetro;
+        private Punto vertice1, vertice2, vertice3, vertice4;
+
+        public Rectangulo(Punto vertice1, Punto vertice3)
+        {
+            this.vertice1 = vertice1;
+            this.vertice3 = vertice3;
+
+            //Paso a crear el vertice 2 tomando la X del v3 y la Y del v1
+            int auxVertice2X = this.vertice3.GetX();
+            int auxVertice2Y = this.vertice1.GetY();
+            this.vertice2 = new Punto(auxVertice2X, auxVertice2Y);
+
+            //Paso a crear el vertice 4 tomando la X del v1 y la Y del v3
+            int auxVertice4X = this.vertice1.GetX();
+            int auxVertice4Y = this.vertice3.GetY();
+            this.vertice4 = new Punto(auxVertice4X, auxVertice4Y);
+        }
+
+        public float Area()
+        {
+            int ver1Y = Math.Abs(this.vertice1.GetY());
+            int ver3X = Math.Abs(this.vertice3.GetX());
+            int ver4X = Math.Abs(this.vertice4.GetX());
+            int ver4Y = Math.Abs(this.vertice4.GetY());
+
+            float baseRect = ver3X - ver4X;
+            float alturaRect = ver1Y - ver4Y;
+
+            setArea(baseRect, alturaRect);
+
+            return GetArea();
+        }
+
+        public float Perimetro()
+        {
+            int ver1Y = Math.Abs(this.vertice1.GetY());
+            int ver3X = Math.Abs(this.vertice3.GetX());
+            int ver4X = Math.Abs(this.vertice4.GetX());
+            int ver4Y = Math.Abs(this.vertice4.GetY());
+
+            float baseRect = ver3X - ver4X;
+            float alturaRect = ver1Y - ver4Y;
+
+            setPerimetro(baseRect, alturaRect);
+
+            return GetPerimetro();
+        }
+
+        private void setArea(float baseRect, float alturaRect)
+        {
+            this.area = baseRect * alturaRect;
+        }
+
+        private void setPerimetro(float baseRect, float alturaRect)
+        {
+            this.perimetro = ((baseRect + alturaRect) * 2);
+        }
+
+        public float GetArea()
+        {
+            return this.area;
+        }
+
+        public float GetPerimetro()
+        {
+            return this.perimetro;
+        }
+    }
+}
