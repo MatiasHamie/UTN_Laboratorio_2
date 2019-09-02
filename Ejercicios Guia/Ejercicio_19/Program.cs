8d3655534fc7efa8f25174fe78187dbdb26f4130
matiashamie <mnhamie@gmail.com>
2019-09-02 16:41:48 -0300

    Ejercicio 19 terminado

---
+++ Ejercicios Guia/Ejercicio_19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sumador a = new Sumador(10);
+            Sumador b = new Sumador();
+
+            Console.WriteLine($"Suma numeros: {a.Sumar(5,6)}");
+            Console.WriteLine($"Suma strings: {b.Sumar("ab","cd")}");
+
+            Console.WriteLine($"Cantidad de sumas iguales? : {a | b}");
+            Console.WriteLine($"Suma cantidad de sumas: {a+b}");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicios Guia/Ejercicio_19/Sumador.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_19
+{
+    class Sumador
+    {
+        private int cantidadSumas;
+
+        public Sumador(int cantidadSumas)
+        {
+            this.cantidadSumas = cantidadSumas;
+        }
+
+        public Sumador():this(0)
+        {
+            /* Al poner el :this(0) nos estamos ahorrando la linea de codigo de abajo
+             * --> this.cantidadSumas = 0;
+             * ya que le estamos pasando al constructor de arriba el 0 como parametro
+             */
+        }
+
+        public static long operator +(Sumador a, Sumador b)
+        {
+            return (long)a.cantidadSumas + b.cantidadSumas;
+        }
+
+        public static bool operator |(Sumador a, Sumador b)
+        {
+            return a.cantidadSumas == b.cantidadSumas;
+        }
+
+        public long Sumar(long a, long b)
+        {
+            this.cantidadSumas++;
+
+            return a + b;
+        }
+
+        public string Sumar(string a, string b)
+        {
+            this.cantidadSumas++;
+
+            return a + b;
+        }
+    }
+}
