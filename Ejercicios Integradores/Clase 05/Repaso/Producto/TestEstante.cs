ce0b742e3d8c275fc868865d4a9eddcfaf68f5f2
MatiasHamie <mnhamie@gmail.com>
2019-09-04 21:27:05 -0300

    Cambios no significativos en el Ej_Clase05

--- Ejercicios Integradores/Clase 05/Repaso/Producto/TestEstante.cs
+++ Ejercicios Integradores/Clase 05/Repaso/Producto/TestEstante.cs
@@ -18,7 +18,7 @@ namespace EjercicioClase5
             Producto p3 = new Producto("Manaos", "MASDS51292", (float)20.5);
             Producto p4 = new Producto("Crush", "CRSDS54861", (float)10.75);
             // Agrego los productos al estante
-            if (estante + p1)
+            if (estante + p1)//Agrega por primera vez p1, "Pepsi"
             {
                 Console.WriteLine("Agregó {0} {1} {2}", p1.GetMarca(), (string)p1, p1.GetPrecio());
             }
@@ -26,7 +26,7 @@ namespace EjercicioClase5
             {
                 Console.WriteLine("¡NO agregó {0} {1} {2}!", p1.GetMarca(), (string)p1, p1.GetPrecio());
             }
-            if (estante + p1)
+            if (estante + p1)//Intenta agregar por segunda vez p1, tiene que fallar porque ya existe
             {
                 Console.WriteLine("Agregó {0} {1} {2}", p1.GetMarca(), (string)p1, p1.GetPrecio());
             }
@@ -34,7 +34,7 @@ namespace EjercicioClase5
             {
                 Console.WriteLine("¡NO agregó {0} {1} {2}!", p1.GetMarca(), (string)p1, p1.GetPrecio());
             }
-            if (estante + p2)
+            if (estante + p2)//Primera vez p2, hay lugar, lo agrega
             {
                 Console.WriteLine("Agregó {0} {1} {2}", p2.GetMarca(), (string)p2, p2.GetPrecio());
             }
@@ -42,7 +42,7 @@ namespace EjercicioClase5
             {
                 Console.WriteLine("¡NO agregó {0} {1} {2}!", p2.GetMarca(), (string)p2, p2.GetPrecio());
             }
-            if (estante + p3)
+            if (estante + p3)//Primera vez p3, hay lugar, lo agrega
             {
                 Console.WriteLine("Agregó {0} {1} {2}", p3.GetMarca(), (string)p3, p3.GetPrecio());
             }
@@ -50,7 +50,7 @@ namespace EjercicioClase5
             {
                 Console.WriteLine("¡NO agregó {0} {1} {2}!", p3.GetMarca(), (string)p3, p3.GetPrecio());
             }
-            if (estante + p4)
+            if (estante + p4)//Primera vez p4, pero el estante fue creado con 3 de cantidad, no hay lugar
             {
                 Console.WriteLine("Agregó {0} {1} {2}", p4.GetMarca(), (string)p4, p4.GetPrecio());
             }
