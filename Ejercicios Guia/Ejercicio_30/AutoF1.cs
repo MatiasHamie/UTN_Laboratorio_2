539af8fcda9346da5095b85634d2c36820a77e8f
matiashamie <mnhamie@gmail.com>
2019-09-20 20:25:29 -0300

    Ejercicio 30

---
+++ Ejercicios Guia/Ejercicio_30/AutoF1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_30
+{
+    class AutoF1
+    {
+        #region Campos
+        private short cantidadCombustible;
+        private short numero;
+        private short vueltasRestantes;
+        private bool enCompetencia;
+        private string escuderia;
+        #endregion
+
+        #region Getters y Setters
+        public short GetCantidadCombustible()
+        {
+            return this.cantidadCombustible;
+        }
+
+        public void SetCantidadCombustible(short cantidadCombustible)
+        {
+            this.cantidadCombustible = cantidadCombustible;
+        }
+
+        public short GetVueltasRestantes()
+        {
+            return this.vueltasRestantes;
+        }
+
+        public void SetVueltasRestantes(short vueltasRestantes)
+        {
+            this.vueltasRestantes = vueltasRestantes;
+        }
+
+        public bool GetEnCompetencia()
+        {
+            return this.enCompetencia;
+        }
+
+        public void SetEnCompetencia(bool enCompetencia)
+        {
+            this.enCompetencia = enCompetencia;
+        }
+
+
+        #endregion
+
+        #region Constructor
+        public AutoF1(short numero, string escuderia)
+        {
+            this.numero = numero;
+            this.escuderia = escuderia;
+        }
+        #endregion
+
+        #region Sobrecarga de Operadores
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            return (a1.escuderia == a2.escuderia) && (a1.numero == a2.numero);
+        }
+
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1 == a2);
+        }
+        #endregion
+
+        #region Métodos
+        public string MostrarDatos()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendFormat("Escuderia: {0}, Numero: {1}, Cant. Combustible: {2} ", this.escuderia, this.numero, this.cantidadCombustible);
+            stringBuilder.Append(this.enCompetencia ? "En Competencia: Si" : "En Competencia: No");
+
+            return stringBuilder.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Ejercicios Guia/Ejercicio_30/Competencia.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_30
+{
+    class Competencia
+    {
+        #region Campos
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<AutoF1> competidores;
+        #endregion
+
+        #region Constructores
+        private Competencia()
+        {
+            competidores = new List<AutoF1>();
+        }
+
+        public Competencia(short cantidadVueltas, short cantidadCompetidores) : this()
+        {
+            this.cantidadVueltas = cantidadVueltas;
+            this.cantidadCompetidores = cantidadCompetidores;
+        }
+        #endregion
+
+        #region Métodos
+        public string MostrarDatos()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("--- Competencia ---");
+            stringBuilder.AppendFormat("Cant. Competidores: {0}, Cant. Vueltas: {1}\n", this.cantidadCompetidores, this.cantidadVueltas);
+            stringBuilder.AppendLine("- Competidores -");
+
+            foreach (AutoF1 auxCompetidor in competidores)
+            {
+                stringBuilder.AppendLine(auxCompetidor.MostrarDatos());
+            }
+
+            return stringBuilder.ToString();
+        }
+        #endregion
+
+        #region Sobrecarga de operadores
+        public static bool operator +(Competencia c, AutoF1 a)
+        {
+            bool retorno = false;
+            
+            if(c.cantidadCompetidores > c.competidores.Count())
+            {
+                if (c != a)//Ya al tener el == y != sobrecargados, ahi tengo el for q se fija si ya existe o no el auto
+                {
+
+                    c.competidores.Add(a);
+                    int indexDelAuto = c.competidores.IndexOf(a);
+                    Random random = new Random();
+                    c.competidores[indexDelAuto].SetEnCompetencia(true);
+                    c.competidores[indexDelAuto].SetCantidadCombustible((short)random.Next(15, 100));
+                    retorno = true;
+                }
+            }
+
+            return retorno;
+        }
+
+        public static bool operator -(Competencia c, AutoF1 a)
+        {
+            bool retorno = false;
+
+            if (c == a)//Ya al tener el == y != sobrecargados, ahi tengo el for q se fija si ya existe o no el auto
+            {
+                c.competidores.Remove(a);
+            }
+
+            return retorno;
+        }
+
+        public static bool operator ==(Competencia c, AutoF1 a)
+        {
+            bool retorno = false;
+
+            for (int i = 0; i < c.competidores.Count(); i++)
+            {
+                if (c.competidores[i] == a)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Competencia c, AutoF1 a)
+        {
+            return !(c == a);
+        }
+        #endregion
+    }
+}
