4d60ea7c22a9ed51a5545a23d8b206c244e6b589
matiashamie <mnhamie@gmail.com>
2019-09-22 22:22:23 -0300

    Ejercicio 31

---
+++ Ejercicios Guia/Ejercicio_31/Cliente.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_31
+{
+    class Cliente
+    {
+        #region Campos
+        private string nombre;
+        private int numero;
+        #endregion
+
+        #region Propiedades
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Numero
+        {
+            get { return numero; }
+        }
+        #endregion
+
+        #region Constructores
+        public Cliente(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public Cliente(int numero, string nombre) : this(numero)
+        {
+            this.nombre = nombre;
+        }
+        #endregion
+
+        #region Sobrecarga de operadores
+        public static bool operator ==(Cliente c1, Cliente c2)
+        {
+            return c1.numero == c2.numero;
+        }
+
+        public static bool operator !=(Cliente c1, Cliente c2)
+        {
+            return !(c1 == c2);
+        }
+        #endregion
+
+    }
+}
---
+++ Ejercicios Guia/Ejercicio_31/Negocio.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_31
+{
+    class Negocio
+    {
+        #region Campos
+        private PuestoAtencion caja;
+        private string nombre;
+        private Queue<Cliente> clientes;
+        #endregion
+
+        #region Propiedades
+        public Cliente Cliente
+        {
+            get { return clientes.Dequeue(); }
+            set { clientes.Enqueue(value); }
+        }
+        public int ClientesPendientes
+        {
+            get { return clientes.Count(); }
+        }
+        #endregion
+
+        #region Constructores
+        private Negocio()
+        {
+            clientes = new Queue<Cliente>();
+            caja = new PuestoAtencion(PuestoAtencion.Puesto.Caja1);
+        }
+
+        public Negocio(string nombre) : this()
+        {
+            this.nombre = nombre;
+        }
+        #endregion
+
+        #region Sobrecarga de operadores
+        public static bool operator ==(Negocio n, Cliente c)
+        {
+            List<Cliente> listaAuxiliar;
+            listaAuxiliar = n.clientes.ToList();
+            bool retorno = false;
+
+            for (int i = 0; i <listaAuxiliar.Count(); i++)
+            {
+                if (listaAuxiliar[i] == c)
+                {
+                    retorno = true;
+                    break;
+                }
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Negocio n, Cliente c)
+        {
+            return !(n == c);
+        }
+
+        public static bool operator +(Negocio n, Cliente c)
+        {
+            bool retorno = false;
+
+            if (n!=c)
+            {
+                n.Cliente = c;//recordar que aca estoy usando el setter implicito de la propertie
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator ~(Negocio n)
+        {
+            return n.caja.Atender(n.Cliente);
+        }
+        #endregion
+    }
+}
